0a11b85455adb15756cac480fa16ae2289bcded6
Jason <therawan.jason@gmail.com>
2025-02-19 22:53:12 +0700

    Add addition function

--- App.java
+++ App.java
@@ -1,8 +1,18 @@
 public class App {
 
+    public int addition(int num1, int num2){
+        return num1 + num2;
+    }
+
+    public App(){
+        int number = addition(5, 10);
+        System.out.println(number);
+    }
+
     public static void main(String[] args) {
         
-        System.out.println("Hello");
+        App app = new App();
+
     }
 
 }
